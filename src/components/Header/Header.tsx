2b372980a8afb4127632a351838c775c7ce23bf6
vijai-ks <vijai.ksv@gmail.com>
2025-06-08 20:52:49 +0530

    feat(ibmatrix): add navbar
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import headerStyles from "./Header.module.scss";
+import IbMatrixLogo from "../../assets/ibMatrixLogo.svg";
+
+const Header = () => {
+  return (
+    <div className={headerStyles.headerContainer}>
+      <div className={headerStyles.headerContents}>
+        <div className={headerStyles.titleContainer}>
+          <img
+            src={IbMatrixLogo}
+            alt="IB Matrix Logo"
+            className={headerStyles.logo}
+          />
+          <div className={headerStyles.title}>IB Matrix</div>
+        </div>
+        <div className={headerStyles.navbar}>
+          <div className={headerStyles.pageName}>Home</div>
+          <div className={headerStyles.pageName}>About Us</div>
+          <div className={headerStyles.pageName}>Services</div>
+          <div className={headerStyles.pageName}>Contact Us</div>
+        </div>
+        <button className={headerStyles.getStartedButton}>GET STARTED</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
